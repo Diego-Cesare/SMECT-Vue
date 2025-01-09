cff58c1475b940f3eb4aa924bb83cafc9c886143
Diego Cesare <diegocesare300491@gmail.com>
2025-01-08 22:54:11 -0300

    Aceter o layout para mobile

--- src/components/MessageBox.vue
+++ src/components/MessageBox.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="w-1/4 h-auto m-4 absolute right-4">
         <Transition name="slide-fade">
-            <div v-if="orderName" class="p-10 bg-indigo-100 rounded-lg">
-                <h1 class="text-xl mb-3">Você selecionou o item <span class="text-pink-400 font-bold">{{ orderName
+            <div v-if="orderName" class="p-10 rounded-lg
+            bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-500">
+                <h1 class="text-xl mb-3 text-white">Você selecionou o item <span class="text-black font-bold">{{ orderName
                         }}</span>
                 </h1>
-                <p>Por favor crie sua lista de pedidos usando apenas</p>
-                <p>itens relacionado á <span class="text-pink-400">{{ orderName }}</span></p>
+                <p class="text-white">Por favor crie sua lista de pedidos usando apenas</p>
+                <p class="text-white">itens relacionado á <span class="text-black">{{ orderName }}</span></p>
             </div>
         </Transition>
         <Transition name="slide-fade">
-            <div v-if="isValidOrder" class="p-10 bg-red-400 rounded-lg">
+            <div v-if="isValidOrder" class="p-10 rounded-lg 
+            bg-gradient-to-r from-red-200 via-red-300 to-red-400">
                 <h1 class="text-xl mb-3 text-white">Você não selecionou nenhum item!!!</h1>
                 <p class="text-white">Por favor, vá até o menu lateral esquerdo e selecione um item para prosseguir com seu pedido!</p>
             </div>
